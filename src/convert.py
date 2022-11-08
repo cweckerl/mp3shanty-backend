ac2b1f5d36d2ca0a28cd6bb6a91ada9933f4cae1
cweckerl <c4rl.hawks@gmail.com>
2022-11-07 21:58:18 -0500

    Album art cropping

--- src/convert.py
+++ src/convert.py
@@ -21,14 +21,54 @@ def convert(id: str, filename: str) -> str:
   yt = YouTube(uri)
   audio = yt.streams.get_audio_only()
   mp4_path = os.path.join("/tmp", f"{filename}.mp4")
-  tmp_file_path = os.path.join("/tmp", "tmp.mp3")
+  cover_path = os.path.join("/tmp", f"{id}.jpg")
+  tmp_file_path = os.path.join("/tmp", f"{id}.mp3")
   mp3_path = os.path.join("/tmp", f"{filename}.mp3")
   audio.download(filename=mp4_path)
-  subprocess.run(["ffmpeg", "-i", mp4_path, "-vn", tmp_file_path])
-  subprocess.run([
-    "ffmpeg", "-i", tmp_file_path, "-i", f"https://img.youtube.com/vi/{id}/maxresdefault.jpg", 
-    "-map", "0:0", "-map", "1:0", "-c", "copy", "-id3v2_version", "3",
-    "-metadata:s:v", 'title="Album cover"',
-    "-metadata:s:v", 'comment="Cover (front)"', "-metadata", f"title={filename}", mp3_path
-  ])
+  # /opt/bin/ffmpeg
+  subprocess.run(
+    [
+      "ffmpeg",
+      "-y",
+      "-i",
+      f"https://img.youtube.com/vi/{id}/maxresdefault.jpg",
+      "-vf",
+      "crop=720:720:280:720",
+      cover_path,
+    ],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT,
+  )
+  subprocess.run(
+    ["ffmpeg", "-y", "-i", mp4_path, "-vn", tmp_file_path],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT,
+  )
+  subprocess.run(
+    [
+      "ffmpeg",
+      "-y",
+      "-i",
+      tmp_file_path,
+      "-i",
+      cover_path,
+      "-map",
+      "0:0",
+      "-map",
+      "1:0",
+      "-c",
+      "copy",
+      "-id3v2_version",
+      "3",
+      "-metadata:s:v",
+      'title="Album cover"',
+      "-metadata:s:v",
+      'comment="Cover (front)"',
+      "-metadata",
+      f"title={filename}",
+      mp3_path,
+    ],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT,
+  )
   return mp3_path
